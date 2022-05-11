61d153d71ab248b085b6003f9357dcdc8ce1dcf6
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-11 14:02:32 +0200

    first steps

--- script.js
+++ script.js
@@ -4,14 +4,30 @@ const app = new Vue({
     el : "#app",
 
     data:{
-        banana : "hello"
+        banana : "hello",
+        emails :[],
+        mailQuantity : 10
     },
 
     methods: {
-        
+        getBooleanApi(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((boolean)=>{
+                    console.log(boolean.data.response)
+                })
+        },
+        for(tot, what){
+            for(i = 0; i< tot; i++){
+                what
+            }
+        }
     },
 
     mounted() {
         console.log(this.banana)
+        this.getBooleanApi()
+        
+
+        
     },
 })
