0f2fc5fe5fd5fbfda719faf0aedf6cc84b490579
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-11 13:56:56 +0200

    start api homework

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log(axios)
+
+const app = new Vue({
+    el : "#app",
+
+    data:{
+        banana : "hello"
+    },
+
+    methods: {
+        
+    },
+
+    mounted() {
+        console.log(this.banana)
+    },
+})
